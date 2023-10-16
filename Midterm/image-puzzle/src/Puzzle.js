a12cd0e717ebf209ef37a7b83b42371025493e69
Cat Almuete <ca2318@nyu.edu>
2023-10-16 14:04:07 -0400

    Bugged puzzle

---
+++ Midterm/image-puzzle/src/Puzzle.js
@@ -0,0 +1,111 @@
+import {useState, useEffect} from 'react'
+import cx from 'classnames'
+import styles from './UI.module.css'
+import Piece1 from './assets/1.png'
+import Piece2 from './assets/2.png'
+import Piece3 from './assets/3.png'
+import Piece4 from './assets/4.png'
+import Piece5 from './assets/5.png'
+import Piece6 from './assets/6.png'
+import Piece7 from './assets/7.png'
+import Piece8 from './assets/8.png'
+
+const pieceImages = [
+  {src: Piece1, matched: false},
+  {src: Piece2, matched: false},
+  {src: Piece3, matched: false},
+  {src: Piece4, matched: false},
+  {src: Piece5, matched: false},
+  {src: Piece6, matched: false},
+  {src: Piece7, matched: false},
+  {src: Piece8, matched: false},
+]
+
+export default function Puzzle(props) {
+  // create some state and setter to store our shuffled cards
+  const [pieces, setPieces] = useState([])
+
+  // Keep track of our choices
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+  const shufflePieces = () => {
+    // spread all of our card images 2x so we have duplicated to match!
+    const shuffledPieces = [...pieceImages]
+      // add a sort function which randomly arranges pieceImages such that each piece only appears once
+      .sort(() => Math.random() - 0.5)
+      // now we map to add another function to add an ID to each item in our new array
+      // first we spread the current properties, then we add an ID
+      .map((piece) => ({...piece, id: Math.random()}))
+    setPieces(shuffledPieces)
+  }
+
+  // handleChoice
+  const handleChoice = (piece) => {
+    // check if we have a choice one
+    choiceOne ? setChoiceTwo(piece) : setChoiceOne(piece)
+  }
+
+  useEffect(() => {
+    // first lets make sure we have both choices
+    if (choiceOne && choiceTwo) {
+      // Map through our pieces and swap the two cards
+      setPieces((prevPieces) => {
+        return prevPieces.map((piece) => {
+          // if the piece is choiceOne, return choiceTwo
+          if (piece === choiceOne) {
+            return choiceTwo
+          } else if (piece === choiceTwo) {
+            // if the piece is choiceTwo, return choiceOne
+            return choiceOne
+          } else {
+            // otherwise return the piece
+            return piece
+          }
+        })
+      })
+      resetTurn()
+    } else {
+      setTimeout(() => resetTurn(), 1000)
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(choiceOne, choiceTwo)
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+  }
+
+  return (
+    <>
+      <button onClick={shufflePieces}>New Game</button>
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          {pieces.map((piece) => (
+            <Piece
+              key={piece.id}
+              piece={piece}
+              handleChoice={handleChoice}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+function Piece(props) {
+  const {piece, handleChoice} = props
+
+  // When you click on a piece perform handleChoice
+  const handleClick = (event) => {
+    handleChoice(piece)
+  }
+
+  return (
+      <div onClick={handleClick}>
+        <img src={piece.src} alt="swap piece" />
+      </div>
+  )
+}
--- Midterm/image-puzzle/src/index.js
+++ Midterm/image-puzzle/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import our React libraries
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import our main App component
+import App from './App'
+
+// Grab our root element from index.html (in public)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// Render our React project within the root element
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
